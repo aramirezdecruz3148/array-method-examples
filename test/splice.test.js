f31fcbd145e05f44dfce605d20ce2e604927063a
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-27 16:25:16 -0700

    two use cases for split with additiona and removal

--- test/splice.test.js
+++ test/splice.test.js
@@ -1,8 +1,18 @@
-const { removedAnElement } = require('../lib/splice');
+const { 
+  removedAnElement, 
+  addAnElement,
+  arrayAfterRemoval,
+  arrayAfterAddition
+} = require('../lib/splice');
 
 describe('splice method tests', () => {
   it('can remove an item from the array', () => {
-    removedAnElement;
     expect(removedAnElement).toEqual([5]);
+    expect(arrayAfterRemoval).toEqual([1, 2, 3]);
+  });
+
+  it('can add an element to the array', () => {
+    expect(addAnElement).toEqual([]);
+    expect(arrayAfterAddition).toEqual([1, 2, 3, 4, 5]);
   });
 });
